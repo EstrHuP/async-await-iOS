8a034ec5ad966cf7a95523f72a6dec766c839970
EstrHuP <estrhup@gmail.com>
2023-06-15 15:12:05 +0200

    Created WebService file

---
+++ AsyncAwaitExample/WebService.swift
@@ -0,0 +1,21 @@
+//
+//  WebService.swift
+//  AsyncAwaitExample
+//
+//  Created by EstrHuP on 15/6/23.
+//
+
+import Foundation
+
+class WebService {
+    
+    private func getDate() async throws -> CurrentDate? {
+        
+        guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
+            fatalError("Url is incorrect!")
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try? JSONDecoder().decode(CurrentDate.self, from: data)
+    }
+}
