a0e3bbd677950b32006ab748ac004e6a67d1c0f1
EstrHuP <estrhup@gmail.com>
2023-06-20 17:50:05 +0200

    Learning how to use Async/Await using Continuation in a playground example

---
+++ AsyncAwaitExample/ContinuationPlayground.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+enum NetworkError: Error {
+    case badUrl
+    case noData
+    case decodingError
+}
+
+struct Post: Decodable {
+    let title: String
+}
+
+func getPosts(completion: @escaping (Result<[Post], NetworkError>) -> Void) {
+    guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+        completion(.failure(NetworkError.badUrl))
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error
+        
+        guard let data = data, error == nil else {
+            completion(.failure(.noData))
+            return
+        }
+        
+        let posts = try? JSONDecoder().decode([Post].self, from: data)
+        completion(.success(posts ?? []))
+        
+    }
+    .resume()
+}
+
+func getPosts() async throws -> [Post] {
+    
+    return try await withCheckedContinuation { continuation in
+        getPosts { result in
+            switch result {
+            case .success(let posts):
+                continuation.resume(returning: posts)
+            case .failure(let error):
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+}
+
+async {
+    do {
+        let posts = try await getPosts()
+        print(posts)
+    } catch {
+        print(error)
+    }
+}
