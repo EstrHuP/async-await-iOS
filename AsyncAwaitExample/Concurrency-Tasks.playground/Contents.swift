ccc1e02269a456c8cecdb4f5cb88e70c11f66110
EstrHuP <estrhup@gmail.com>
2023-06-21 21:16:54 +0200

    Created a new Playground and use the Async-let Tasks
    Understanding structured concurrency in swift

---
+++ AsyncAwaitExample/Concurrency-Tasks.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+enum NetworkError: Error {
+    case badUrl
+    case decodingError
+}
+
+struct CreditScore: Decodable {
+    let score: Int
+}
+
+struct Constants {
+    struct Urls {
+        static func equifax(userId: Int) -> URL? {
+            return URL(string: "https://ember-sparkly-rule.glitch.me/equifax/credit-score/\(userId)")
+        }
+        
+        static func experian(userId: Int) -> URL? {
+            return URL(string: "https://ember-sparkly-rule.glitch.me/experian/credit-score/\(userId)")
+        }
+    }
+}
+
+func calculateAPR(creditScores: [CreditScore]) -> Double {
+    let sum = creditScores.reduce(0) { next, credit in
+        return next + credit.score
+    }
+    
+    return Double((sum/creditScores.count)/100)
+}
+
+func getARP(userId: Int) async throws -> Double {
+    guard let equifaxURL = Constants.Urls.equifax(userId: userId), let experianURL = Constants.Urls.experian(userId: userId) else {
+        throw NetworkError.badUrl
+    }
+    
+    //Do task concurrencly and more easier to do test
+    
+    async let (equifaxData, _) = URLSession.shared.data(from: equifaxURL)
+    async let (experianData, _) = URLSession.shared.data(from: experianURL)
+    
+    let equifaxCreditScore = try? JSONDecoder().decode(CreditScore.self, from: try await equifaxData)
+    
+    let experianCreditScore = try? JSONDecoder().decode(CreditScore.self, from: try await experianData)
+    
+    guard let equifaxCreditScore = equifaxCreditScore, let experianCreditScore = experianCreditScore else {
+        throw NetworkError.decodingError
+    }
+
+    return calculateAPR(creditScores: [equifaxCreditScore, experianCreditScore])
+}
+
+Task {
+    let apr = try await getARP(userId: 1)
+    print(apr)
+}
